ca39be440ee72e5a664dd749f41e97c3c15581b4
david fletcher <davidfletcheraaron@gmail.com>
2022-01-19 14:57:44 -0500

    err handling midd

--- api/server.js
+++ api/server.js
@@ -7,4 +7,11 @@ const server = express();
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
+server.use((err, req, res, next)=>{
+   res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack
+   })
+})
+
 module.exports = server;
